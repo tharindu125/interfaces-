cfe30161a3e5f9e9caeb46f2c39c8d084a1022be
tharindu125 <nawanjanaenda@gmail.com>
2022-09-18 00:51:42 +0530

    interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import prof from "./image/prof.png";
+import email from "./image/email.png";
+import pw from "./image/pw.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className='sub_main'>
+      <div>
+          <div className='img'>
+
+              <div className='container_img'>
+                <img src={prof} alt="prof" className='prof'/> 
+                   
+              </div>
+          </div>
+          <div >
+                <h1 >Login page</h1>
+                <div>
+                  <img src={email} alt="email" className='email'/>
+                  <input type="text" placeholder="user name" className='name'/>
+                </div>
+
+                <div className='second_input'>
+                  <img src={pw} alt="pw" className='pw'/>
+                  <input type="password" placeholder="password" className='name '/>
+                </div>
+              <div className='login_button'>
+                <button>Login</button>
+              </div>
+
+              
+                <p className='link'>
+                  <a href='#'>Forgot password</a> or <a href='#'>Sign-up</a>
+                </p>
+              
+                
+          </div> 
+      </div>
+      </div>
     </div>
   );
 }
